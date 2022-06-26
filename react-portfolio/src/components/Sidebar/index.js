22d09cda8104ec6de7dc8f5c8153a462cbb058b3
Preda Alexandru <alexpreda95@yahoo.com>
2022-06-26 13:59:30 +0300

    Created Sidebar

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,62 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import Logo from '../../assets/images/MyLogo.png'
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className='navbar'>
+      <Link className='logo' to='/'>
+        <img src={Logo} alt="logo" />
+      </Link>
+      <h1 className='subtitle'>Alex Preda</h1>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/alexandru-preda-80a6ba158/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/predaalexandru"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:alex_predas" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
